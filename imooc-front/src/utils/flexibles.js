76ba63ca069e84d8871075ea0f660432d20886c1
Hailie17 <192151452@qq.com>
2024-07-09 23:35:12 +0800

    定义相对路径

--- imooc-front/src/utils/flexibles.js
+++ imooc-front/src/utils/flexibles.js
@@ -1,9 +1,11 @@
 import { computed } from 'vue'
 import { PC_DEVICE_WIDTH } from '../constants'
+import { useWindowSize } from '@vueuse/core'
 
 /**
  * 判断当前是否为 移动设备，屏幕宽度是否小于指定宽度（1280）
  */
+const { width } = useWindowSize()
 export const isMobileTerminal = computed(() => {
-  return document.documentElement.clientWidth < PC_DEVICE_WIDTH
+  return width.value < PC_DEVICE_WIDTH
 })
