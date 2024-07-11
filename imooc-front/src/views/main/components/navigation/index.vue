80c8176e4e7403666441dd360daa7c979145919a
Hailie17 <192151452@qq.com>
2024-07-11 23:54:32 +0800

    1

--- imooc-front/src/views/main/components/navigation/index.vue
+++ imooc-front/src/views/main/components/navigation/index.vue
@@ -5,6 +5,15 @@
 <script setup>
 import { isMobileTerminal } from '@/utils/flexibles'
 import MobileNavigationVue from './mobile/index.vue'
+import { getCategoryList } from '@/api/category'
+import { ref } from 'vue'
+// 11
+const categorys = ref([])
+const getCategoryData = async () => {
+  const { categorys } = await getCategoryList()
+  categorys.value = categorys
+}
+getCategoryData()
 </script>
 
 <style lang="scss" scoped></style>
--- imooc-front/src/views/main/components/navigation/mobile/index.vue
+++ imooc-front/src/views/main/components/navigation/mobile/index.vue
@@ -2,13 +2,6 @@
   <div class="">移动端 navigation</div>
 </template>
 
-<script setup>
-import { getCategoryList } from '@/api/category'
-const getCategoryData = async () => {
-  const res = await getCategoryList()
-  console.log('res', res)
-}
-getCategoryData()
-</script>
+<script setup></script>
 
 <style lang="scss" scoped></style>
